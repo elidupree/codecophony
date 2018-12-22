7544f0774a52bb862097a05598bf2bf149c297b6
Eli Dupree <vcs@elidupree.com>
2018-12-22 12:08:12 -0500

    Made rudimentary sound playing using Codecophony rendering

--- codecophony_editor_backend/src/main.rs
+++ codecophony_editor_backend/src/main.rs
@@ -1,13 +1,22 @@
 extern crate codecophony;
 extern crate codecophony_editor_shared as shared;
 extern crate serde_json;
+extern crate portaudio;
+extern crate dsp;
 
-use std::thread;
+//use std::thread;
 use std::io::{self, BufRead, Write};
 //use std::time::Duration;
 
+use dsp::sample::ToFrameSliceMut;
+use portaudio::{PortAudio, Stream, StreamSettings};
+
+use codecophony::Renderable;
 use shared::{MessageToBackend, MessageToFrontend};
 
+type Output = f32;
+const CHANNELS: usize = 2;
+const SAMPLE_HZ: f64 = 44100.0;
 
 fn main() {
   //println!("Hello from backend (stdout)");
@@ -16,6 +25,28 @@ fn main() {
   let stdin = stdin.lock();
   let stdout = io::stdout();
   let mut stdout = stdout.lock();
+  
+  let callback = move |portaudio::OutputStreamCallbackArgs { buffer, .. }| {
+    let buffer: &mut [[Output; CHANNELS]] = buffer.to_frame_slice_mut().unwrap();
+    dsp::slice::equilibrium(buffer);
+      
+    let position = 0;
+    let note = codecophony::MIDIPercussionNote::new (0.0, 10.0, 100, 36);
+    Renderable::<[Output; CHANNELS]>::render(&note, buffer, position, SAMPLE_HZ);
+    
+    portaudio::Continue
+  };
+  
+  let pa = portaudio::PortAudio::new().unwrap();
+  let settings = pa.default_output_stream_settings::<Output>(
+    CHANNELS as i32,
+    SAMPLE_HZ,
+    4096, // frames per buffer
+  ).unwrap();
+  let mut stream = pa.open_non_blocking_stream(settings, callback).unwrap();
+  stream.start().unwrap();
+
+  
   for line in stdin.lines() {
     let line = line.unwrap();
     if let Ok(message) = serde_json::from_str(&line) {
@@ -33,3 +64,4 @@ fn main() {
     //thread::sleep(Duration::from_millis(50));
   }
 }
+
